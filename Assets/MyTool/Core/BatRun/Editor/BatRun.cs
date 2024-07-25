42cbea5807d22a782ec338ec5e029b37f01bc9a3
MaHongyu <jiangnan_mahongyu@163.com>
2024-07-25 15:58:38 +0800

    新http

--- Assets/MyTool/Core/BatRun/Editor/BatRun.cs
+++ Assets/MyTool/Core/BatRun/Editor/BatRun.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 namespace MhyTool
@@ -118,6 +120,119 @@ namespace MhyTool
                     Debug.LogFormat("Exception Occurred :{0},{1}", ex.Message, ex.StackTrace.ToString());
                 }
             }
+        }
+
+
+        public static void Get(string url, Action<string> action)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            req.Timeout = 3000;
+
+            Debug.LogError($"req {url}");
+            if (req == null || req.GetResponse() == null)
+                return;
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            if (resp == null)
+                return;
+
+            using (Stream stream = resp.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    // Debug.LogError(reader.ReadToEnd());
+                    action(reader.ReadToEnd());
+                }
+            }
+        }
+
+        [Serializable]
+        public class ReplayDetail
+        {
+            public string replay_id;
+            public int time;
+            public string lua_ver;
+            public string replay_ver;
+            public long start_tick;
+        }
+        [Serializable]
+        public class ReplayData
+        {
+            public List<ReplayDetail> replay_data_time = new List<ReplayDetail>();
+        }
+
+        const string  replayListUtl = "http://aquaman-battle.ingress-pre.flowgame.cn/replaysvr/public/getReplayTime?replay_time_cost=1";
+        const string  replayDataUrl = "http://aquaman-battle.ingress-pre.flowgame.cn/replaysvr/public/getReplayDataPb?replay_id=";
+
+        const string  prod_replayListUtl = "http://aquaman-battle.ingress-prod.flowgame.cn/replaysvr/public/getReplayTime?replay_time_cost=5000";
+        const string  prod_replayDataUrl = "http://aquaman-battle.ingress-prod.flowgame.cn/replaysvr/public/getReplayDataPb?replay_id=";
+
+        [MenuItem("MyTool/TestHttp")]
+        static void getData()
+        {
+            try
+            {
+                Get(replayListUtl, (str) =>
+                {
+                    OnGetDat("pre_replay",str, replayDataUrl);
+                });
+                // Get(prod_replayListUtl, (str) =>
+                // {
+                //     OnGetDat("prod_replay",str, prod_replayDataUrl);
+                // });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("错误：\n{0}", ex.Message);
+            }
+        }
+
+
+
+        public static void OnGetDat(string root, string str, string replayDataUrl)
+        {
+            Debug.LogError(str);
+            var data = JsonUtility.FromJson<ReplayData>(str);
+            Dictionary<string, List<string>> paths = new Dictionary<string, List<string>>();
+            foreach (var detail in data.replay_data_time)
+            {
+                string timeStep = (Mathf.Floor(detail.time / 1000f/ 5f) * 5).ToString();
+                var filename = $"D:\\{root}\\{detail.lua_ver}\\{detail.lua_ver}_{detail.time}_{detail.replay_id}.data";
+                if (!Directory.Exists(Path.GetDirectoryName(filename)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(filename));
+                }
+
+                if (!File.Exists(filename))
+                {
+                    Debug.LogError($"new at {filename}");
+                    var url = replayDataUrl + detail.replay_id;
+                    System.Net.HttpWebRequest Myrq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(url);
+                    System.Net.HttpWebResponse myrp = (System.Net.HttpWebResponse)Myrq.GetResponse();
+                    System.IO.Stream st = myrp.GetResponseStream();
+                    System.IO.Stream so = new System.IO.FileStream(filename, System.IO.FileMode.Create);
+                    byte[] by = new byte[1024];
+                    int osize = st.Read(by, 0, (int)by.Length);
+                    while (osize > 0)
+                    {
+                        so.Write(by, 0, osize);
+                        osize = st.Read(by, 0, (int)by.Length);
+                    }
+                    so.Close();
+                    st.Close();
+                    myrp.Close();
+                    Myrq.Abort();
+                }
+                if (!paths.ContainsKey(detail.lua_ver))
+                {
+                    paths.Add(detail.lua_ver,new List<string>());
+                }
+                paths[detail.lua_ver].Add(filename.Replace("\\","\\\\"));
+            }
+
+            var export = new Framework.Editor.CueEditor.DataToLuaHelper();
+            export.ExportDataToLua(paths, "replays", Framework.Resource.ResConfig.EditorLuaClientDataPath);
         }
     }
 }
--- Assets/MyTool/Core/BatRun/SvnUpdate.bat
+++ Assets/MyTool/Core/BatRun/SvnUpdate.bat
@@ -6,7 +6,17 @@ svn up D:\AqumanClientClean --accept  theirs-full
 svn revert D:\AqumanClientClean -R
 svn status D:\AqumanClientClean
 
+svn cleanup D:\AquamanClientMilestone02
+svn up D:\AquamanClientMilestone02 --accept  theirs-full
+svn revert D:\AquamanClientMilestone02 -R
+svn status D:\AquamanClientMilestone02
+
+svn cleanup D:\AquamanDesigMilestone02
+svn up D:\AquamanDesigMilestone02 --accept  theirs-full
+svn revert D:\AquamanDesigMilestone02 -R
+svn status D:\AquamanDesigMilestone02
+
 svn cleanup D:\AquamanDesign\table_export
-svn up D:\AquamanDesign\table_export --accept theirs-full
+svn up D:\AquamanDesign\table_export --accept  theirs-full
 ::svn revert D:\AquamanDesign\table_export -R
 svn status D:\AquamanDesign\table_export
